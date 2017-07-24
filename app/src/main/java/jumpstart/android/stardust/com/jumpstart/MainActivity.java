58a86b1228fb601a54c3d884aa85f9429dd97c36
Li Cheng <chengli@thoughtworks.com>
2017-07-24 16:50:26 +0800

    Init project

---
+++ app/src/main/java/jumpstart/android/stardust/com/jumpstart/MainActivity.java
@@ -0,0 +1,13 @@
+package jumpstart.android.stardust.com.jumpstart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
